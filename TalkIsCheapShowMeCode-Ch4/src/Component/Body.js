d13b01f550934c26acba6b636b7c89322f3de82c
Mansi Jain <70169492+Mansijain06@users.noreply.github.com>
2023-01-12 19:19:46 +0530

    ch-5

--- TalkIsCheapShowMeCode-Ch4/src/Component/Body.js
+++ TalkIsCheapShowMeCode-Ch4/src/Component/Body.js
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import { resturantList } from "../config";
 import ResturantCard from "./ResturantCard";
 
+const filterData = (searchText, resturants) => {
+  const filteredData = resturants.filter((resturant) =>
+    resturant.data.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filteredData;
+};
+
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [resturants, setResturants] = useState(resturantList);
   return (
-    <div className="resturant-list">
-      {resturantList.map((resturant) => {
-        return <ResturantCard {...resturant.data} key={resturant.data.id} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-button"
+          onClick={(e) => {
+            const resturantData = resturantList;
+            const data = filterData(searchText, resturantData);
+            setResturants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="resturant-list">
+        {resturants.map((resturant) => {
+          return <ResturantCard {...resturant.data} key={resturant.data.id} />;
+        })}
+      </div>
+    </>
   );
 };
 
