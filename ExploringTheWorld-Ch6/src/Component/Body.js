69975a0710e6a06eae581120348efe67153aabb4
Mansi Jain <70169492+Mansijain06@users.noreply.github.com>
2023-01-15 23:13:50 +0530

    ch6

---
+++ ExploringTheWorld-Ch6/src/Component/Body.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { resturantList } from "../config";
+import ResturantCard from "./ResturantCard";
+import Shimmer from "./Shimmer";
+
+const filterData = (searchText, resturants) => {
+  const filteredData = resturants.filter((resturant) =>
+    resturant.data.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filteredData;
+};
+
+const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [filteredResturants, setFilteredResturants] = useState([]);
+  const [allResturants, setAllResturants] = useState([]);
+
+  useEffect(() => {
+    getResturants();
+  }, []);
+
+  async function getResturants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.73651&lng=75.889219&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    if (data.ok) {
+      const json = await data.json();
+      setAllResturants(json?.data?.cards[2]?.data?.data?.cards);
+      setFilteredResturants(json?.data?.cards[2]?.data?.data?.cards);
+    } else {
+      return Promise.reject("Oops! something went wrong " + data.status);
+    }
+  }
+
+  // if (filteredResturants?.length == 0) {
+  //   return <h1>No restaurants match your filter!</h1>;
+  // }
+
+  return !allResturants.length ? (
+    <Shimmer />
+  ) : (
+    <div className="body-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter name of resturant"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-button"
+          onClick={(e) => {
+            const data = filterData(searchText, allResturants);
+            setFilteredResturants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      {!filteredResturants?.length ? (
+        <h1>No restaurants match your filter!</h1>
+      ) : (
+        <div className="resturant-list">
+          {filteredResturants.map((resturant) => {
+            return (
+              <ResturantCard {...resturant.data} key={resturant.data.id} />
+            );
+          })}
+        </div>
+      )}
+      {/* {((a = 10), console.log(a))} */}
+    </div>
+  );
+};
+
+export default Body;
---
+++ ExploringTheWorld-Ch6/src/Component/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        alt="Food Villa"
+        src="https://lh3.googleusercontent.com/p/AF1QipMf9w4RomHXrUkQKvrxtPdjp3SLadP05HDzXlH2=w1080-h608-p-no-v0"
+      />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Contact Us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
